7718d588ddbc362ff47f3dfec4a8ce9eaa369aaa
JornToomingas <jmarkus@hkhk.edu.ee>
2024-10-04 11:17:33 +0300

    Add files via upload

---
+++ ülesanne1.py
@@ -0,0 +1,18 @@
+# Jörn Markus Toomingas
+# 04.10.24
+# Harjutus01
+
+print('"Cabernet Sauvignon" või \'Cabernet Sauvignon\'?')
+print('  @..@')
+print(' (----)')
+print('( \__/ )')
+print('^^ "" ^^')
+print()
+
+#int täisarv 123
+#str sõne "tekst"
+#float kommarv 1.23
+
+a = int(input("Lisa ruudu külje pikkus: "))
+s = a ** 2 #astendamine
+print("Vastus: ruudu pindala on ",s)
