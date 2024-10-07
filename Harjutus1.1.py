fccaf676c054873e20b7597791781fc8201c519e
JornToomingas <jmarkus@hkhk.edu.ee>
2024-10-07 15:30:40 +0300

    Add files via upload

---
+++ Harjutus1.1.py
@@ -0,0 +1,48 @@
+# Jörn Markus Toomingas
+#07.10.24
+#Harjutus 01.1
+
+import turtle
+"""
+#muudan asukohta
+turtle.penup()
+turtle.goto(-300,140)
+turtle.pendown()
+#kolmnurk
+turtle.showturtle()
+turtle.shape("turtle")
+
+
+turtle.fd(200)
+turtle.left(120)
+turtle.fd(200)
+turtle.left(120)
+turtle.fd(200)
+turtle.left(120)
+"""
+"""
+#muudan asukohta
+turtle.penup()
+turtle.goto(-200,-180)
+turtle.pendown()
+
+#J-täht
+turtle.setheading(270)
+turtle.fd(150)
+turtle.circle(-50,180)
+turtle.right(90)
+turtle.fd(10)
+turtle.right(90)
+turtle.circle(40,180)
+turtle.fd(150)
+turtle.left(90)
+turtle.fd(70)
+turtle.right(90)
+turtle.fd(10)
+turtle.right(90)
+turtle.fd(80)
+turtle.right(90)
+turtle.fd(10)
+
+turtle.done()
+"""
---
+++ Ülesanne1.1.py
@@ -0,0 +1,18 @@
+#Jörn Markus Toomingas
+#07.10.24
+#Ülesanne 1
+
+import turtle
+
+turtle.fd(200)
+turtle.right(144)
+turtle.fd(200)
+turtle.right(144)
+turtle.fd(200)
+turtle.right(144)
+turtle.fd(200)
+turtle.right(144)
+turtle.fd(200)
+turtle.right(144)
+
+turtle.done()
