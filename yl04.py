4701978f2eb7733bc5b61594601e19b6716d594a
JornToomingas <jmarkus@hkhk.edu.ee>
2024-10-16 11:46:13 +0300

    Create yl04.py

---
+++ yl04.py
@@ -0,0 +1,19 @@
+#16.10.24 Toomingas
+#Ülesanded 04
+
+
+#2. Raamatute allahindlus
+ale = 0.3
+kogus = 3
+hind = 12.53
+summa = hind - (hind * ale) * kogus
+print(f"{kogus} raamatu hind soodustustega on {summa: 0.2f}€")
+
+
+
+
+#1. Aita pikkus
+a = int(input("Sisesta külg 1 "))
+b = int(input("Sisesta külg 2 "))
+P = 2 * (a+b)
+print(f"Aia kogupikkus on {P} meetrit.")
