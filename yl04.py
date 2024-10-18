332b10ea755723611ad63feaa549a4ce68cfcc32
JornToomingas <jmarkus@hkhk.edu.ee>
2024-10-18 10:56:13 +0300

    Update yl04.py

--- yl04.py
+++ yl04.py
@@ -1,5 +1,40 @@
 #16.10.24 Toomingas
 #Ülesanded 04
+import turtle
+
+#5. Ringi pindala ja Turtle
+try:
+    r = int(input("Lisa ringi raadius:  "))
+    pi = 3.14
+    S = pi * r**2  #Astendamine
+    P = 2*pi*r
+    print(f"Ringi pindala on {S:.2f} ja ümbermõõt on {P:.2f}")
+    turtle.circle(r, 360)
+except:
+    print("Tegid ssestamisel vea!")
+
+turtle.done
+
+
+#4. Kingituste pakkimine
+try:
+    kingitused = int(input("Lisa kingituste arv:  "))
+    maht = 5
+    pakid = kingitused // maht
+    yle = kingitused % maht
+    print(f"Saad teha {pakid} täis kinkekasti. Üle jääb {yle} kingitust:  ")
+except: 
+    print("Tegid sisestamisel vea!")
+
+
+#3. Faili allalaadimine
+try: 
+    failisuurus = int(input("Sisesta faili suurus (MB): "))
+    downkiirus = int(input("Sisesta allalaadimisekiirus (MB/s): "))
+    aeg = failisuurus / downkiirus
+    print(f"Allalaadimiseks kulub {aeg:0.2f} sekundit")
+except:
+    print("Tegid sisestamisel vea!")
 
 
 #2. Raamatute allahindlus
@@ -8,8 +43,6 @@ kogus = 3
 hind = 12.53
 summa = hind - (hind * ale) * kogus
 print(f"{kogus} raamatu hind soodustustega on {summa: 0.2f}€")
-
-
 
 
 #1. Aita pikkus
