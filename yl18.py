3fb3d19cf61b1de099d8a3b7c682c3e1e10ce244
JornToomingas <jmarkus@hkhk.edu.ee>
2025-01-15 11:41:53 +0200

    Update yl18.py

--- yl18.py
+++ yl18.py
@@ -3,10 +3,24 @@
 import csv
 
 faili_nimi = 'EstonianBasketballGames.csv'
-
+meeskonnad = {}
 with open(faili_nimi, mode='r', encoding='utf-8') as fail:
 
     dict_reader = csv.DictReader(fail)
 
     for rida in dict_reader:
-        meeskonnad = int(rida["Team 1"])
+        meesk1 = rida["Team 1"]
+        meesk2 = rida["Team 2"]
+        if meesk1 not in meeskonnad:
+            meeskonnad[meesk1] = 0
+        if meesk2 not in meeskonnad:
+            meeskonnad[meesk2] = 0
+
+        if meesk1 in meeskonnad:
+            meeskonnad[meesk1]+=1
+        if meesk2 in meeskonnad:
+            meeskonnad[meesk2]+=1
+
+
+
+print(f"osales {len(meeskonnad)} meeskonda")
