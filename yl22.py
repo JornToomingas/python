fb7c18a80c697610d793bd7df5ef8036bd6001c7
JornToomingas <jmarkus@hkhk.edu.ee>
2025-01-28 14:55:18 +0200

    Update yl22.py

--- yl22.py
+++ yl22.py
@@ -1,7 +1,12 @@
 #28.01.25 Jörn Markus Toomingas
 #Ülesanne 22
+
+
 import locale
 from datetime import datetime, date
+import csv
+
+"""
 #1. Lihtne kuupäev
 now = datetime.now()
 synniaeg = datetime(2008, 7, 2)
@@ -9,5 +14,13 @@ print("Praegune aeg: ", now.strftime("%d.%m.%Y %H:%M:%S"))
 difference = now - synniaeg
 print("Kuupäevade vahe päevades: ", difference.days)
 print("Kuupäevade vahe aastates: ", int(difference.days/365), "aastat")
+"""
 
 #2. Autorent
+faili_nimi = 'rentals.csv'
+
+with open(faili_nimi, mode='r', encoding='utf-8') as fail:
+    csv_lugeja = csv.reader(fail)
+
+    for rida in csv_lugeja:
+        print(rida)
